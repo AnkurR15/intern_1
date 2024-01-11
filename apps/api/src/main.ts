baeef72fbae0ed74555e0fd3603d97da390c3f1c
Ankur <redijankur2002@gmail.com>
2024-01-11 10:30:12 +0530

    swagger added

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,17 +5,32 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  const config = new DocumentBuilder()
+    .setTitle('My App')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('myapp')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+  Logger.log(
+    `🚀 Swagger UI is running on: http://localhost:${port}/api-docs`
   );
 }
 
